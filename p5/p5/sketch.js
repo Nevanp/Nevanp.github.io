0a10a45a1325699f1db22ee86a80ad277e323a42
Nevanp <43014130+Nevanp@users.noreply.github.com>
2018-09-10 14:06:18 -0600

    p5js

---
+++ p5/p5/sketch.js
@@ -0,0 +1,31 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+let x = 50;
+let y = 50;
+let ySpeed = 5;
+let xSpeed = 5;
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+  background(255, 0, 255);
+ if(keyIsDown(LEFT_ARROW)){
+  x = x -xSpeed;
+}
+if(keyIsDown(RIGHT_ARROW)){
+ x = x + xSpeed;
+}
+if(keyIsDown(DOWN_ARROW)){
+  y = y + ySpeed;
+}
+if(keyIsDown(UP_ARROW)){
+ y = y - ySpeed;
+}
+    ellipse(x, y, 50, 50);
+}
