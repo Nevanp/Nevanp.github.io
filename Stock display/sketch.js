9dab43442996e3f1700137b677df635297eca035
Nevanp <43014130+Nevanp@users.noreply.github.com>
2018-10-16 13:04:45 -0600

    Make it work

--- Stock display/sketch.js
+++ Stock display/sketch.js
@@ -1,11 +1,11 @@
 // Project Title
-// Your Name
-// Date
+// Nevan Parsley
+// 10/16/18
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let stockOne;
+let stockOne = "";
 let state = 0;
 let input, button, info;
 let minutes;
@@ -19,7 +19,7 @@ let hours;
 // }
 
 
-function setup() {
+function setup() {   //Set up input box
   createCanvas(windowWidth, windowHeight);
   background(255);
   minutes = minute();
@@ -33,7 +33,7 @@ function setup() {
 }
 
 
-function draw() {
+function draw() { //grab current time to the nearest hour and minute and change states/display text
   while(minutes % 5 !== 0){
     minutes -= 1;
   }
@@ -48,34 +48,46 @@ function draw() {
     getStock();
   }
   else{
+    background(255);
     wait();
+    let i=0;
+    for (let info in stockOne) {
+      if (stockOne.hasOwnProperty(info)) {
+        text(info + ":", width/2-200, height/2 - 100 + i);
+        text(stockOne[info], width/2 + 200, height/2 - 100 + i);
+      }
+      i += 60;
+    }
+    // text(stockOne, width/2, height/2);
   }
 }
 
 
-function getStock(){
+function getStock(){//load stock JSON
   background(255);
     let getUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + input.value() + "&interval=5min&outputsize=full&apikey=KZF5VVHCEAG08NU6";
-    let displayStock = loadJSON(getUrl);
-    stockOne = displayStock["Time Series (5min)"]["2018-10-" + day() + " "  + hours + ":" + minutes + ":" + "00"];
-    text(stockOne, width/2, height/2);
+    loadJSON(getUrl, showStock);
     state = 2;
 }
 
 
-function menu(){
+function menu(){ //display text menu
   background(255);
-  textSize(100);
+  textSize(65);
   textAlign(CENTER);
   text("Enter in stock id in all caps to search stock", width/2, height/2);
 }
 
-function changeState(){
+function changeState(){ //lets search button change text
   state = 1;
 }
 
-function wait(){
+function wait(){ //take back to menu
   if(keyIsPressed){
     state = 0;
   }
+}
+function showStock(theStock){ //loads stock to variable so you can use it
+  stockOne = theStock["Time Series (5min)"]["2018-10-" + day() + " "  + hours + ":" + minutes + ":" + "00"];
+
 }
