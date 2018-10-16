6aff750be3ec8bdda8d7d566be33bcee329cba8c
BuildTools <unconfigured@null.spigotmc.org>
2018-10-15 22:44:13 -0600

    Make display in text

--- p5-7/sketch.js
+++ p5-7/sketch.js
@@ -6,75 +6,76 @@
 // - describe what you did to take this project "above and beyond"
 
 let stockOne;
-let stockTwo;
 let state = 0;
-let input, button, greeting;
-
-
-function preload(){
-  let urlAapl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + "AAPL" + "&interval=5min&outputsize=full&apikey=KZF5VVHCEAG08NU6";
-  let urlMsft = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + "MSFT" + "&interval=5min&outputsize=full&apikey=KZF5VVHCEAG08NU6";
-  stockOne = loadJSON(urlAapl);
-  stockTwo = loadJSON(urlMsft);
-}
+let input, button, info;
+let minutes;
+let hours;
+//
+// function preload(){
+//   let urlAapl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + "AAPL" + "&interval=5min&outputsize=full&apikey=KZF5VVHCEAG08NU6";
+//   let urlMsft = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + "MSFT" + "&interval=5min&outputsize=full&apikey=KZF5VVHCEAG08NU6";
+//   stockOne = loadJSON(urlAapl);
+//   stockTwo = loadJSON(urlMsft);
+// }
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-   input = createInput();
+  background(255);
+  minutes = minute();
+  hours = hour();
+  input = createInput();
    input.position(20, 65);
 
-   button = createButton('submit');
+   button = createButton("search");
    button.position(input.x + input.width, 65);
-   button.mousePressed(greet);
-
-   greeting = createElement('h2', 'what is your name?');
-   greeting.position(20, 5);
-
-   textAlign(CENTER);
-   textSize(50);
+   button.mouseClicked(changeState);
 }
 
 
 function draw() {
-
-  // if(state === 0){
-  //   menu();
-  // }
-  // else if(state === 1){
-  //   getStock();
-  // }
+  while(minutes % 5 !== 0){
+    minutes -= 1;
+  }
+  if (hours > 16 || hours < 9){
+    hours = 16;
+    minutes = "00";
+  }
+  if(state === 0){
+    menu();
+  }
+  else if(state === 1){
+    getStock();
+  }
+  else{
+    wait();
+  }
 }
 
 
-// function getStock(){
-//
-// }
-
-
-// function mouseClicked(){
-//   if(mouseX > width/2){
-//     state = 1;
-//     background(0);
-//   }
-//   // else if(mouseX < width/2){
-//   //   state = 2;
-//   //   background(0);
-//   // }
-// }
+function getStock(){
+  background(255);
+    let getUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + input.value() + "&interval=5min&outputsize=full&apikey=KZF5VVHCEAG08NU6";
+    let displayStock = loadJSON(getUrl);
+    stockOne = displayStock["Time Series (5min)"]["2018-10-" + day() + " "  + hours + ":" + minutes + ":" + "00"];
+    text(stockOne, width/2, height/2);
+    state = 2;
+}
 
 
 function menu(){
-  input = createInput();
-   input.position(20, 65);
+  background(255);
+  textSize(100);
+  textAlign(CENTER);
+  text("Enter in stock id in all caps to search stock", width/2, height/2);
+}
 
-   button = createButton('submit');
-   button.position(input.x + input.width, 65);
-   button.mousePressed(greet);
+function changeState(){
+  state = 1;
+}
 
-   greeting = createElement('h2', 'what is your name?');
-   greeting.position(20, 5);
-
-   textAlign(CENTER);
-   textSize(50);
+function wait(){
+  if(keyIsPressed){
+    state = 0;
+  }
 }
